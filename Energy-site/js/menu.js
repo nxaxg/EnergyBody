26a55b5970fbe9d5ea625213b66dba8e71123350
N. A <nxayancanxg@gmail.com>
2016-10-02 17:22:59 -0300

    VI
    franquicia done 😎

--- Energy-site/js/menu.js
+++ Energy-site/js/menu.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
         });
     });
     
-    //slider
+    //slider icons
     $(".slider-group").sss({
         slideShow : false, // Set to false to prevent SSS from automatically animating.
         startOn : 0, // Slide to display first. Uses array notation (0 = first slide).
@@ -16,8 +16,16 @@ $(document).ready(function(){
         speed : 3500, // Slideshow speed in milliseconds.
         showNav : true // Set to false to hide navigation arrows.
     });
-    
+    //slider-sucursal
     $(".slider-group-sucursal").sss({
+        slideShow : true, // Set to false to prevent SSS from automatically animating.
+        startOn : 0, // Slide to display first. Uses array notation (0 = first slide).
+        transition : 400, // Length (in milliseconds) of the fade transition.
+        speed : 5000, // Slideshow speed in milliseconds.
+        showNav : true // Set to false to hide navigation arrows.
+    });
+    //slider-tesimonial
+    $(".slider-testimonial").sss({
         slideShow : true, // Set to false to prevent SSS from automatically animating.
         startOn : 0, // Slide to display first. Uses array notation (0 = first slide).
         transition : 400, // Length (in milliseconds) of the fade transition.
