b85bed52fc10695c89fbb17ca103efb8bb3271b4
N. A <nxayancanxg@gmail.com>
2016-10-03 17:36:16 -0300

    VIII
    Front end done.

---
+++ Energy-site/PHP/footer.php
@@ -0,0 +1,29 @@
+<section class="subfooter">
+    <div class="container-fluid">
+        <div class="row">
+            <ul class="rs-grid list-inline col-lg-4 col-lg-offset-4 col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 col-xs-8 col-xs-offset-2">
+                <li class="col-lg-3 col-md-3 col-sm-3 col-xs-3 text-center">
+                    <a href="#" class="fa fa-facebook"></a>
+                </li>
+                <li class="col-lg-3 col-md-3 col-sm-3 col-xs-3 text-center">
+                    <a href="#" class="fa fa-twitter"></a>
+                </li>
+                <li class="col-lg-3 col-md-3 col-sm-3 col-xs-3 text-center">
+                    <a href="#" class="fa fa-instagram"></a>
+                </li>
+                <li class="col-lg-3 col-md-3 col-sm-3 col-xs-3 text-center">
+                    <a href="#" class="fa fa-youtube-play"></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</section>
+<footer>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="copyright col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
+                <p>&copy;Copyright <a href="#">Energybody</a> 2016</p>
+            </div>
+        </div>
+    </div>
+</footer>
